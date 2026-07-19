6b8fbcd368c861f765af1478e6240927776917da
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2026-07-19 02:09:03 +0000

    [Auto] Update Hash & Version

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 { pkgs, appimageTools, copyDesktopItems, makeDesktopItem, ... }:
 let
   pname = "beeper";
-  version = "4.2.972";
+  version = "4.2.985";
   src = pkgs.fetchurl {
     url = "https://beeper-desktop.download.beeper.com/builds/Beeper-${version}-x86_64.AppImage";
-    hash = "sha256-luMwF1yCc3ja0AFVNkWU9QMdSEiyH7u25MaVT/+3BQQ=";
+    hash = "sha256-oWJdpZL+Q8/jaI/WJfgXUisPASuvHkxU6rOeJkedHSM=";
   };
 
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
