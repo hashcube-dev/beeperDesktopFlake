5b08a9ba25b3c08861b66cf324f774ac558ba1c0
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2026-09-06 02:29:20 +0000

    [Auto] Update Hash & Version

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 { pkgs, appimageTools, copyDesktopItems, makeDesktopItem, ... }:
 let
   pname = "beeper";
-  version = "4.3.73";
+  version = "4.3.89";
   src = pkgs.fetchurl {
     url = "https://beeper-desktop.download.beeper.com/builds/Beeper-${version}-x86_64.AppImage";
-    hash = "sha256-j6HTTqU7+z8AJ20hYgia6wOH0/n3gxrUocCpMPmgQvQ=";
+    hash = "sha256-p95gWugC7BQDeoiibAxem7Jw3GCuv91NYif7n+QfXFk=";
   };
 
   # Beeper's current AppImage stores its AI2 marker at offset 1024,
